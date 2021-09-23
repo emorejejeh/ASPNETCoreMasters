1673f7f419c7ee7907297ecf7f77922725a43f04
Jerome Agrimano <JeromeA@magenic.com>
2021-09-23 23:40:01 +0800

    Session 4

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -1,13 +1,60 @@
 ﻿using Services.DTO;
 using Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Services
 {
     public class ItemService : IItemService
     {
+        private readonly IEnumerable<ItemDto> _items;
+
+         
+        public ItemService()
+        {
+            _items = CreateDto(2);
+        }
         public void Save(ItemDto dto)
         {
 
         }
+
+        public IEnumerable<ItemDto> GetAll()
+        {
+            return _items;
+        }
+
+        public ItemDto GetById(int id)
+        {
+            return _items.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public void UpdateItem(int id)
+        {
+
+        }
+
+        public void DeleteItem(int id)
+        {
+
+        }
+
+
+        public IEnumerable<ItemDto> CreateDto(int count)
+        {
+            var response = new List<ItemDto>();
+            for(int x = 1; x <= count; x++)
+            {
+                response.Add(new ItemDto
+                {
+                    Id = x,
+                    Item = "Item" + x.ToString()
+                });
+            }
+
+            return response;
+        }
+
+
     }
 }
